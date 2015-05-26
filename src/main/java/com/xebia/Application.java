3307deb86a143f3be4518bb87965dacbb540f809
njozwiak <njozwiak@xebia.fr>
2015-05-26 19:45:48 +0200

    Add hystrix dashboard

--- src/main/java/com/xebia/Application.java
+++ src/main/java/com/xebia/Application.java
@@ -3,11 +3,13 @@ package com.xebia;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class Application {
 
     public static void main(String[] args) throws InterruptedException {
